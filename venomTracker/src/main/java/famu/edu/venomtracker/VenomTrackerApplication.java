034cecc784341710d60372a824abc8da8d75f8ca
jaden1harris <112529468+jaden1harris@users.noreply.github.com>
2022-10-18 19:45:20 -0400

    Added VenomTrackerApplication

---
+++ venomTracker/src/main/java/famu/edu/venomtracker/VenomTrackerApplication.java
@@ -0,0 +1,13 @@
+package famu.edu.venomtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenomTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenomTrackerApplication.class, args);
+    }
+
+}
