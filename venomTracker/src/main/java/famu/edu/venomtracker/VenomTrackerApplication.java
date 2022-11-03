822a9c681923b5609db4c5b2eb69b5e45f9d593c
Amia303 <34247519+Amia303@users.noreply.github.com>
2022-11-02 20:57:06 -0400

    update

--- venomTracker/src/main/java/famu/edu/venomtracker/VenomTrackerApplication.java
+++ venomTracker/src/main/java/famu/edu/venomtracker/VenomTrackerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VenomTrackerApplication {
-
+import java.util.Scanner;
     public static void main(String[] args)
     {
         SpringApplication.run(VenomTrackerApplication.class, args);
